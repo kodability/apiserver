10d1da14e9493c6a96202acefc09453cdda256e3
LechuckRoh <member@lechuck2011.cafe24.com>
2018-02-01 22:54:27 +0900

    database automigration added

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"log"
 	"tryout-runner/db"
+	"tryout-runner/models"
 	_ "tryout-runner/routers"
 
 	"github.com/astaxie/beego"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+func migrate() {
+	db.Conn.AutoMigrate(&models.Question{},
+		&models.QuestionDesc{},
+		&models.QuestionCode{},
+		&models.QuestionTag{},
+	)
+}
+
 func main() {
 	conn, err := db.Connect()
 	if err != nil {
 		log.Fatal("Failed to connect to DB", err)
 	}
+
+	migrate()
 
 	defer conn.Close()
 
