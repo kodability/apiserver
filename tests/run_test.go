7b70f81ae530d4c0a9dffc2d90e878a7c775d5c2
Lechuck Roh <member@lechuck2011.cafe24.com>
2018-03-14 22:39:34 +0900

    run tryout test added

--- tests/run_test.go
+++ tests/run_test.go
@@ -3,7 +3,9 @@ package tests
 import (
 	"testing"
 
-	"github.com/kodability/tryout-runner/controllers"
+	c "github.com/kodability/tryout-runner/controllers"
+	"github.com/kodability/tryout-runner/db"
+	m "github.com/kodability/tryout-runner/models"
 	_ "github.com/kodability/tryout-runner/routers"
 
 	"github.com/astaxie/beego"
@@ -11,14 +13,75 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func deleteTryouts() {
+	conn := db.Conn
+	conn.Unscoped().Delete(m.Tryout{})
+}
+
+func createGroovyQuestionCode() m.QuestionCode {
+	return m.QuestionCode{
+		Lang:     "groovy",
+		InitCode: "",
+		TestCode: `
+import static org.testng.AssertJUnit.*
+import org.testng.annotations.*
+
+class TestExample {
+	@Test(timeOut= 1000L)
+	void test1() {
+		assertEquals(55, new Example().sum(1, 10))
+	}
+	void test2() {
+		assertEquals(1, new Example().sum(1, 1))
+	}
+}`,
+	}
+}
+
 func TestRun(t *testing.T) {
-	runBody := controllers.RunBody{QuestionID: 1, Code: ""}
-	r, w, _ := makePostJSON("/api/v1/run", runBody)
-	beego.BeeApp.Handlers.ServeHTTP(w, r)
+	deleteQuestionsAndDescAndCodes()
+
+	// Create a question
+	groovyQuestionCode := createGroovyQuestionCode()
+	question := m.Question{
+		Codes: []m.QuestionCode{groovyQuestionCode},
+	}
+	db.Conn.Create(&question)
 
 	Convey("POST run\n", t, func() {
+		deleteTryouts()
+
+		runBody := c.RunBody{
+			QuestionID: question.ID,
+			Lang:       groovyQuestionCode.Lang,
+			Code: `
+			class Example {
+				int sum(int from, int to) {
+					(from + to) * (to - from + 1) / 2
+				}
+			}`,
+		}
+		r, w, _ := makePostJSON("/api/v1/run", runBody)
+		beego.BeeApp.Handlers.ServeHTTP(w, r)
+
 		Convey("StatusCode = 201", func() {
 			So(w.Code, ShouldEqual, 201)
+		})
+		Convey("Inserted Tryout", func() {
+			var tryouts []m.Tryout
+			db.Conn.Find(&tryouts)
+			So(tryouts, ShouldHaveLength, 1)
+
+			tryout := tryouts[0]
+			So(map[string]interface{}{
+				"QuestionID": tryout.QuestionID,
+				"Lang":       tryout.Lang,
+				"Code":       tryout.Code,
+			}, ShouldResemble, map[string]interface{}{
+				"QuestionID": question.ID,
+				"Lang":       groovyQuestionCode.Lang,
+				"Code":       runBody.Code,
+			})
 		})
 	})
 }
